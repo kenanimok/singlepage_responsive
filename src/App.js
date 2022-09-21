2477c39b587d993e2da2301e1c08e28334108e52
Kenanimok <touch@sutha-MacBook-Pro.local>
2022-09-21 16:56:25 +0700

    fix feature serveice

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Navbar } from "./components";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./globalStyles";
 import Home from "./pages/HomePage/Home";
+import Service from "./pages/Service/Service";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Service />} />
       </Routes>
     </BrowserRouter>
   );
